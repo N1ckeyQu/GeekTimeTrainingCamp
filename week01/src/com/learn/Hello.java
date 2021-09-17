1cec11378e689c1d5697d211f7779de3b2becec7
= <=>
2021-09-17 14:57:57 +0800

    解决乱码问题

--- week01/src/com/learn/Hello.java
+++ week01/src/com/learn/Hello.java
@@ -1,7 +1,7 @@
-//package com.learn;
-//
-//public class Hello {
-//    static {
-//        System.out.println("你好，世界！");
-//    }
-//}
+package com.learn;
+
+public class Hello {
+    static {
+        System.out.println("你好，世界！");
+    }
+}
